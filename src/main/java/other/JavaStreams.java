a791325c5a02c6da9fa76c47cdcbca257534de7e
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-06-06 12:12:43 +0530

    added insert at the beginning of linked list

--- src/main/java/other/JavaStreams.java
+++ src/main/java/other/JavaStreams.java
@@ -1,12 +1,13 @@
 package other;
 
 import java.io.IOException;
-import java.util.stream.IntStream;
+//import java.util.stream.IntStream;
+
 
 public class JavaStreams {
 
     public static void main(String[] args) throws IOException {
         //1. Integer stream
-        IntStream.range(1, 10).forEach(System.out::print);
+       // IntStream.range(1, 10).forEach(System.out::print);
     }
 }
