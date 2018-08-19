06dc00f12ec5333e437932d8ffbae49c54be4cbd
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-08-19 12:10:29 +0530

    formatting

--- src/main/java/doublylinkedlist/operations/InsertIntoDoublyLL.java
+++ src/main/java/doublylinkedlist/operations/InsertIntoDoublyLL.java
@@ -1,25 +1,26 @@
 package doublylinkedlist.operations;
 
 public class InsertIntoDoublyLL {
-  // adding item at the beginning
-  public static DLLNode insetAtBeginning(DLLNode head, int data) {
-    if (head == null || head.next == null)
-      System.out.println("List is empty or it is having only one element.");
-    DLLNode newNode = new DLLNode(data, null);
-    newNode.next = head;
-    head.prev = newNode;
-    return newNode;
-  }
-  // adding item at the end of list
-  public static DLLNode insertAtEnd(DLLNode head, int data) {
-    if (head == null || head.next == null)
-      System.out.println("List is empty or it is having only one element.");
-    DLLNode ptr1;
-    while (head.next != null) {
-      ptr1 = head;
-      head = head.next;
+    // adding item at the beginning
+    public static DLLNode insetAtBeginning(DLLNode head, int data) {
+        if (head == null || head.next == null)
+            System.out.println("List is empty or it is having only one element.");
+        DLLNode newNode = new DLLNode(data, null);
+        newNode.next = head;
+        head.prev = newNode;
+        return newNode;
     }
 
-    return null;
-  }
+    // adding item at the end of list
+    public static DLLNode insertAtEnd(DLLNode head, int data) {
+        if (head == null || head.next == null)
+            System.out.println("List is empty or it is having only one element.");
+        DLLNode ptr1;
+        while (head.next != null) {
+            ptr1 = head;
+            head = head.next;
+        }
+
+        return null;
+    }
 }
