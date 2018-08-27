7aa3bc0a9769067f5aed7bffb42bf667e81a5f4a
Mohanty, Nirmal <Nirmal.Mohanty@cdk.com>
2018-08-27 23:48:33 +0530

    DLL insert at the end

--- src/main/java/LinkedListAlgo/doublylinkedlist/DriverProgram.java
+++ src/main/java/LinkedListAlgo/doublylinkedlist/DriverProgram.java
@@ -5,25 +5,24 @@ import LinkedListAlgo.doublylinkedlist.operations.InsertIntoDoublyLL;
 import LinkedListAlgo.doublylinkedlist.operations.PrintingDoublyLL;
 
 public class DriverProgram {
-  static DLLNode head;
+    static DLLNode head;
 
-  public static void main(String[] args) {
-    DriverProgram list = new DriverProgram();
-    list.head = new DLLNode(10, null);
-    list.head.next = new DLLNode(20, list.head);
-    list.head.next.next = new DLLNode(30, list.head.next);
-    list.head.next.next.next = new DLLNode(40, list.head.next.next);
-    list.head.next.next.next.next = new DLLNode(50, list.head.next.next.next);
-//Printing list
-    PrintingDoublyLL.printDoublyLL(head);
-    //Insert at beginning of the list
-    DLLNode modifiedList= InsertIntoDoublyLL.insetAtBeginning(head,5);
-    PrintingDoublyLL.printDoublyLL(modifiedList);
+    public static void main(String[] args) {
+        DriverProgram list = new DriverProgram();
+        list.head = new DLLNode(10, null);
+        list.head.next = new DLLNode(20, list.head);
+        list.head.next.next = new DLLNode(30, list.head.next);
+        list.head.next.next.next = new DLLNode(40, list.head.next.next);
+        list.head.next.next.next.next = new DLLNode(50, list.head.next.next.next);
+        //Printing list
+        PrintingDoublyLL.printDoublyLL(head);
+        //Insert at beginning of the list
+        DLLNode modifiedList = InsertIntoDoublyLL.insetAtBeginning(head, 0);
+        PrintingDoublyLL.printDoublyLL(modifiedList);
+        //Insert at the end of list
+        PrintingDoublyLL.printDoublyLL( InsertIntoDoublyLL.insertAtEnd(modifiedList, 60));
 
 
-
-
-
-  }
+    }
 }
 
--- src/main/java/LinkedListAlgo/doublylinkedlist/operations/InsertIntoDoublyLL.java
+++ src/main/java/LinkedListAlgo/doublylinkedlist/operations/InsertIntoDoublyLL.java
@@ -13,14 +13,19 @@ public class InsertIntoDoublyLL {
 
     // adding item at the end of list
     public static DLLNode insertAtEnd(DLLNode head, int data) {
+        //Creating a node out of the give value
+        DLLNode lastNode = new DLLNode(data, null);
+        //Linked list null check
         if (head == null || head.next == null)
             System.out.println("List is empty or it is having only one element.");
-        DLLNode ptr1;
+        DLLNode ptr1=head;
         while (head.next != null) {
-            ptr1 = head;
             head = head.next;
         }
+        head.next = lastNode;
+        lastNode.prev = head;
+        lastNode.next = null;
 
-        return null;
+        return ptr1;
     }
 }
